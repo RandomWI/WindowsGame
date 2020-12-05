0034ac67a0522cf4c68688b39df5523be19f6774
Baxi1114 <baxi1114@gmail.com>
2020-12-05 22:16:10 +0100

    Clean code

--- window-prt-master/GUI_windows/src/GUI_WindowsApp/ButtonBar.java
+++ window-prt-master/GUI_windows/src/GUI_WindowsApp/ButtonBar.java
@@ -25,20 +25,6 @@ public class ButtonBar extends JPanel {
     public ButtonBar(){
         super(new GridBagLayout());
 
-        /*JButton buttonSave = new JButton("Mentés");
-        JButton buttonLoaLoading = new JButton("Betöltés");
-
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor = GridBagConstraints.WEST;
-        constraints.insets = new Insets(2, 2, 2, 2);
-
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        add(buttonSave, constraints);
-
-        constraints.gridx = 1;
-        add(buttonLoaLoading, constraints);*/
-        
         setButtonBar();
         
         buttonSave.addActionListener(new ActionListener() {
